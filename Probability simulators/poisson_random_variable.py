7c0a9167a140f1681c2dbfad157d3847bc5c7f50
arnavydv <aarnavyadvv@gmail.com>
2026-05-24 16:57:56 +0530

    binomalrv added

--- Probability simulators/poisson_random_variable.py
+++ Probability simulators/poisson_random_variable.py
@@ -7,4 +7,11 @@ def poisson_pmf(average_rate:float,event_to_find_probability:float)->float:
     secondterm=(average_rate**event_to_find_probability)/math.factorial(event_to_find_probability)
     return exp*secondterm
 
-
+def binomial_pmf(success:float,number_of_trails:float,proability:float)->float:
+    """binomial random variable tells about the 
+    probability of success only like what is the probability of two heads 
+    out of theree trials"""
+    choosing=math.factorial(number_of_trails)/(math.factorial(success)*math.factorial(number_of_trails-success))
+    second_term=proability**success
+    third_term=(1-proability)**(number_of_trails-success)
+    return choosing*second_term*third_term
