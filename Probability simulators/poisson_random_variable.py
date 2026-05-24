10b4f35485b839581fe7306637f47dad6987e754
arnavydv <aarnavyadvv@gmail.com>
2026-05-24 13:12:47 +0530

    added some more examples and clear code

--- Probability simulators/poisson_random_variable.py
+++ Probability simulators/poisson_random_variable.py
@@ -1,10 +1,10 @@
 import math 
-def poisson_rv(lam:float,event:float)->float:
+def poisson_pmf(average_rate:float,event_to_find_probability:float)->float:
     """poisson random variable tells us about 
     somethings is happening at an average rate 
     so it tell probability of an event based on average rate"""
-    exp= (math.e**(-lam))
-    secondterm=(lam**event)/math.factorial(event)
+    exp= (math.e**(-average_rate))
+    secondterm=(average_rate**event_to_find_probability)/math.factorial(event_to_find_probability)
     return exp*secondterm
 
 
--- Probability simulators/test.py
+++ Probability simulators/test.py
@@ -1,3 +1,6 @@
 from poisson_random_variable import poisson_rv
 print(poisson_rv(lam=3,event=4))
 #its tells that if 3persons/hr coming to a cafe then probability of 4 persons coming to a cafe is 
+# other example is 
+""" if i am doing 5 hours of work daily what is probability that i will do the work for 12 hours next day"""
+print(poisson_rv(lam=5,event=12))#0.00343424028557
